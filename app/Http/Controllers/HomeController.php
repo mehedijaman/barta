c070e7dfaec5f06d4ab5c76058a1c219b62c0797
Mehedi Jaman <mail4mjaman@gmail.com>
2023-11-20 13:04:06 +0600

    - some modification

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,13 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use Inertia\Inertia;
-use App\Models\User;
+use Illuminate\Http\Response;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $posts = Post::with(['author','comments.author'])->orderBy('created_at', 'desc')->get();
+        $posts = Post::with(['author','comments.author'])->orderBy('created_at', 'desc')->paginate();
+
+        if($request->wantsJson()){
+            error_log('Request wants JSON');
+            return [];
+        }
+
         return Inertia::render('Home', ['posts' => $posts]);
     }
 }
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,9 +3,35 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import CreatePost from '@/Pages/Post/CreatePost.vue';
 import PostCard from '@/Pages/Post/PostCard.vue';
+import { ref, onMounted, onUnmounted } from 'vue';
+import { debounce } from 'lodash/function';
 
 const props = defineProps({
     posts:Object
+});
+
+const posts = ref(props.posts.data);
+
+async function nextPosts(){
+    // const res = await fetch(props.posts.next_page_url);
+    // const data = await res.json();
+    console.log(props.posts.links.next);
+    // posts.value = [...posts.value, ...data.data];
+}
+
+onMounted(() => {
+    const handleScroll = debounce((e) => {
+        const pixelsFromBottom = document.documentElement.offsetHeight - document.documentElement.scrollTop - window.innerHeight;
+        if(pixelsFromBottom < 200 ){
+            nextPosts();
+        }
+    }, 100);
+
+    window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('scroll', handleScroll);
 });
 </script>
 
@@ -15,7 +41,7 @@ const props = defineProps({
     <AuthenticatedLayout>
         <CreatePost></CreatePost>
         <TransitionGroup name="fade">
-            <PostCard v-for="post in props.posts" :key="post.id" :post="post"></PostCard>
+            <PostCard v-for="post in posts" :key="post.id" :post="post"></PostCard>
         </TransitionGroup>
     </AuthenticatedLayout>
 </template>
