8002b430d96ed5a98c92f0df7cc8ed4715c75741
Mehedi Jaman <mail4mjaman@gmail.com>
2023-11-22 13:54:43 +0600

    Fix: - Searcher whenever hit enter button

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -56,7 +56,7 @@ function search() {
                                                     clip-rule="evenodd"></path>
                                             </svg>
                                         </div>
-                                        <input v-model="searchText" @change.prevent="search()" type="text"
+                                        <input v-model="searchText" @keyup.enter="search()" type="text"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-2xl focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                             placeholder="Search...">
                                     </div>
